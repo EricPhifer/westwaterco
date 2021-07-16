46221acf47f059fc67eeb8a804bf2cf218133316
Eric Phifer <ericphifer@Erics-MacBook-Pro.local>
2021-07-16 06:01:36 -0600

    updating homepage styles

--- gatsby-ww/src/pages/index.js
+++ gatsby-ww/src/pages/index.js
@@ -2,18 +2,24 @@ import { graphql, Link } from 'gatsby';
 // import SanityImage from 'gatsby-plugin-sanity-image';
 import React from 'react';
 import styled from 'styled-components';
+import { FaRegArrowAltCircleDown } from 'react-icons/fa';
 import SEO from '../components/SEO';
 import logo from '../assets/images/logo.png';
 
 const HomeStyles = styled.div`
   text-align: center;
+  height: 89vh;
   .hero {
     display: grid;
     grid-template-areas:
       '. . . . logo logo logo logo . . . .'
       '. . . . title title title title . . . .'
       '. . . . tagline tagline tagline tagline . . . .'
-      '. . . . links links links links . . . .';
+      '. . . . links links links links . . . .'
+      '. . . . . icon icon . . . . .';
+    #logo {
+      grid-area: logo;
+    }
     .logoContainer {
       place-self: center center;
     }
@@ -25,23 +31,85 @@ const HomeStyles = styled.div`
       background-repeat: no-repeat;
       background-position: center center;
     }
-    #logo {
-      grid-area: logo;
-    }
     #title {
       grid-area: title;
+    }
+    .heroTitle {
+      font-size: 4rem;
+      text-transform: uppercase;
+      text-shadow: 0px 0px 2px #f7f9fb;
+      font-weight: 500;
     }
     #tagline {
       grid-area: tagline;
     }
+    .heroTagline {
+      font-size: 2rem;
+      text-shadow: #fff 2px 2px 5px;
+      margin-top: 0;
+      margin-bottom: 0.9rem;
+    }
     #links {
       grid-area: links;
     }
-    #projectLink {
-      grid-area: projectLink;
+    .heroLinks {
+      list-style: none;
+      line-height: 1;
+      color: #fff;
+      fill: #fff;
+      text-align: center;
+      padding-left: 0;
+      li {
+        display: inline-block;
+        margin: 2.3rem 0 5rem 2.2rem;
+        -webkit-transition: all 0.3s;
+        -o-transition: all 0.3s;
+        transition: all 0.3s;
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
+      .buttonesque {
+        text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.48);
+        background-color: #5085a5;
+        border-style: solid;
+        border-width: 1px;
+        border-color: #7a7a7a;
+        border-radius: 0;
+        box-shadow: 8px 5px 10px 0px rgba(0, 0, 0, 0.5);
+        font-size: 13px;
+        padding: 10px 20px;
+        -webkit-transition: all 0.3s;
+        -o-transition: all 0.3s;
+        transition: all 0.3s;
+        &:hover {
+          color: rgb(93, 109, 89);
+          background-color: rgb(246, 248, 250);
+          border-color: #fff;
+        }
+      }
+      a {
+        text-decoration: none;
+        text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.48);
+        &:hover {
+          color: inherit;
+        }
+      }
     }
-    #contactLink {
-      grid-area: contactLink;
+    #icon {
+      grid-area: icon;
+    }
+    @keyframes bounce {
+      from {
+        transform: translateY(0);
+      }
+      to {
+        transform: translateY(5px);
+      }
+    }
+    .heroIcon {
+      font-size: 5rem;
+      animation: bounce 0.7s ease-in-out infinite alternate;
     }
   }
   @media (max-width: 400px) {
@@ -76,26 +144,31 @@ export default function HomePage({ data }) {
           <div id="title" className="heroTitle">
             Consulting Engineers and Scientists
           </div>
-          <div id="tagline" className="heroTagline">
+          <p id="tagline" className="heroTagline">
             We'll handle the engineering and environmental consulting needs you
             have for your project.
-          </div>
+          </p>
           <ul id="links" className="heroLinks">
             <li id="projectLink">
-              <Link to="/projects" role="button">
-                <span className="buttonesqe">
+              <Link to="/projects" role="button" className="buttonesque">
+                <span>
                   <span>See Our Projects</span>
                 </span>
               </Link>
             </li>
             <li id="contactLink">
-              <Link to="/about#contact" role="button">
-                <span className="buttonesqe">
+              <Link to="/about#contact" role="button" className="buttonesque">
+                <span>
                   <span>Contact Us</span>
                 </span>
               </Link>
             </li>
           </ul>
+          <div id="icon" className="iconContainer">
+            <div className="heroIcon">
+              <FaRegArrowAltCircleDown />
+            </div>
+          </div>
         </div>
       </HomeStyles>
     </>
